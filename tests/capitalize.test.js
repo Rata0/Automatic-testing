f4afb97e39375412e4429e297dabe465882b84f4
enid <nekon689gmail.com>
2023-01-16 16:42:06 +0300

    file add

---
+++ tests/capitalize.test.js
@@ -0,0 +1,18 @@
+const capitalize = (text) => {
+    if (text === '') {
+      return '';
+    }
+    const firstChar = text[0].toUpperCase();
+    const restSubstring = text.slice(1);
+    return `${firstChar}${restSubstring}`;
+  };
+
+if (capitalize('hello') !== 'Hello') {
+    throw new Error('Функция работает неверно!');
+  }
+  
+if (capitalize('') !== '') {
+    throw new Error('Функция работает неверно!');
+  }
+  
+  console.log('Все тесты пройдены!');
